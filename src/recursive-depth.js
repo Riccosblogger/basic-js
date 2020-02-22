10d2518ba700e48ed874ac859c27b1c4917f2864
Riccosblogger <riccosblogger@gmail.com>
2020-02-22 14:06:03 +0300

    feat: add the solution for subtask 8
    List of solutions:
    8. Recursive depth calculator

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,11 +1,9 @@
 module.exports = class DepthCalculator {
-    calculateDepth(arr,  depth=1) {
-        //depth++;
-        //alert (max_depth);      
-        if (!Array.isArray(arr)) return;
-        else for (let value of arr) {
-            alert ('value=' + value + 'depth=' + depth);
-            if (Array.isArray(value)) calculateDepth(value, ++depth); }
-            return depth;	
-        }
-};
+    calculateDepth(arr, depth = 0) {
+      for (const value of arr) {
+        if(Array.isArray(value))
+          depth = Math.max(this.calculateDepth(value), depth);
+      }
+      return ++depth;
+    }
+  }
